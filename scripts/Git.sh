abed3d749002c76b37b01d7338e1b0ba929c55be
Samuel Gaudreault <sam.gaudr@gmail.com>
2020-11-23 20:27:56 -0500

    Add git script

---
+++ scripts/Git.sh
@@ -0,0 +1,31 @@
+
+echo -e "${COLOR}Installing Git...${NOCOLOR}"
+
+git_user_name=$(set_parameter "Git user name" "")
+git_user_email=$(set_parameter "Git user email" "")
+git_credential_helper="store"
+git_core_editor="nano"
+bashrc="$(eval echo ~${SUDO_USER})/.bashrc"
+
+apt install git -y
+
+echo -e "${COLOR}Configuring Git...${NOCOLOR}"
+git config --global user.name "$git_user_name"
+git config --global user.email "$git_user_email"
+git config --global credential.helper $git_credential_helper
+git config --global core.editor $git_core_editor
+
+
+echo -e "${COLOR}Replacing PS1...${NOCOLOR}"
+
+# Add git_parse_branch function 
+touch temp 
+cat ./config/parse_git_branch >> temp
+cat $bashrc >> temp
+mv temp $bashrc
+
+
+#Replace PS1
+sed -i -ne '/if \[ "\$color_prompt" = yes \]; then/ {p; r ./config/ps1' -e ':a; n; /fi/ {p; b}; ba}; p' $bashrc
+
+echo -e "${COLOR}Done!${NOCOLOR}"
